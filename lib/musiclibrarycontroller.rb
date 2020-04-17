0b4b1cd34307e1c0e6d0d0f4037565cb5fa504ce
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-04-17 12:31:35 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -7,7 +7,7 @@ class MusicLibraryController
 
   def call
      input = ''
- 
+
      while input != 'exit'
        puts "Welcome to your music library!"
        puts "To list all of your songs, enter 'list songs'."
@@ -19,7 +19,7 @@ class MusicLibraryController
        puts "To quit, type 'exit'."
        puts "What would you like to do?"
        input = gets.chomp
- 
+
        case input
        when 'list songs'
          self.list_songs
@@ -39,9 +39,9 @@ class MusicLibraryController
          "Type in a valid request please"
        end
      end
- 
+
    end
- 
+
    def list_songs
      Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
        puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
@@ -49,58 +49,58 @@ class MusicLibraryController
      # Song.all.each_with_index {|song, i| puts "#{i+1}. #{song.artist} - #{song.name} - #{song.genre}"}
      # binding.pry
    end
- 
+
    def list_artists
      artists = Artist.all.sort {|a,b| a.name <=> b.name}
      artists.each.with_index(1) {|artist, i| puts "#{i}. #{artist.name}"}
    end
- 
+
    def list_genres
      genres = Genre.all.sort {|a,b| a.name <=> b.name}
      genres.each.with_index(1) {|genre, i| puts "#{i}. #{genre.name}"}
    end
- 
+
    # binding.pry
    def list_songs_by_artist
      puts "Please enter the name of an artist:"
      input = gets.strip
- 
+
      if artist = Artist.find_by_name(input)
        artist.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, i|
          puts "#{i}. #{song.name} - #{song.genre.name}"
        end
      end
    end
- 
+
    def list_songs_by_genre
      puts "Please enter the name of a genre:"
      input = gets.strip
- 
+
      if genre = Genre.find_by_name(input)
        genre.songs.sort { |a,b| a.name <=> b.name }.each.with_index(1) do |song, i|
          puts "#{i}. #{song.artist.name} - #{song.name}"
        end
      end
    end
- 
+
    def play_song
      puts "Which song number would you like to play?"
      input = gets.chomp.to_i
- 
+
      # self.list_songs
      songs = Song.all
      # songs.sort {|a,b| a.name <=> b.name}
- 
- 
- 
+
+
+
      if (1..songs.length).include?(input)
        song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
      end
      # binding.pry
- 
+
      puts "Playing #{song.name} by #{song.artist.name}" if song
    end
- 
- 
+
+
  end
 end
